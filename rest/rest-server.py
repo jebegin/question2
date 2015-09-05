bf53c3cdaa19d73d3ece75f16a097f7e5115126e
Jacob Begin <jbegin@gridcosystems.com>
2015-09-05 12:38:25 -0400

    Completed rest methods

--- rest/rest-server.py
+++ rest/rest-server.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
+#{"sha_id":1,"count":12,"good_ips":2,"bad_ips":1}
+
 events = [
     {
         'sha_id': 1,
@@ -27,47 +29,38 @@ def get_event(sha_id):
 def create_event():
     print 'Content-Type: %s' % request.headers['Content-Type']
 
-    #if not request.json or not 'sha_id' in request.json:
-    #    abort(400)
-    #event = {
-    #    'sha_id': request.json['sha_id'],
-    #    'count': request.json['count'],
-    #    'good_ips': request.json['good_ips'],
-    #    'bad_ips': request.json['bad_ips']
-    #}
-    #events.append(event)
-    #return jsonify({'event': event}), 201
-
-    if 'application/octet-stream' in request.headers['Content-Type']:
-        return 'figure out how to use proto'
-        #event = {
-        #    'sha_id': request.json['sha_id'],
-        #    'count': request.json['count'],
-        #    'good_ips': request.json['good_ips'],
-        #    'bad_ips': request.json['bad_ips']
-        #}
-        #events.append(event)
-        #return jsonify({'event': event}), 201
+    if 'application/json' in request.headers['Content-Type']:
+        event = {
+            'sha_id': request.json['sha_id'],
+            'count': request.json['count'],
+            'good_ips': request.json['good_ips'],
+            'bad_ips': request.json['bad_ips']
+        }
+        events.append(event)
+        return jsonify({'event': event}), 201
     else:
         abort(400)
 
-#@app.route('/messages', methods = ['POST'])
-#def api_message():
-#
-#    if request.headers['Content-Type'] == 'text/plain':
-#        return "Text Message: " + request.data
-#
-#    elif request.headers['Content-Type'] == 'application/json':
-#        return "JSON Message: " + json.dumps(request.json)
-#
-#    elif request.headers['Content-Type'] == 'application/octet-stream':
-#        f = open('./binary', 'wb')
-#        f.write(request.data)
-#        f.close()
-#        return "Binary message written!"
-#
-#    else:
-#        return "415 Unsupported Media Type ;)"
+    #if 'application/octet-stream' in request.headers['Content-Type']:
+    #    return 'figure out how to use proto'
+    #    #event = {
+    #    #    'sha_id': request.json['sha_id'],
+    #    #    'count': request.json['count'],
+    #    #    'good_ips': request.json['good_ips'],
+    #    #    'bad_ips': request.json['bad_ips']
+    #    #}
+    #    #events.append(event)
+    #    #return jsonify({'event': event}), 201
+    #else:
+    #    abort(400)
+
+@app.route('/events/<int:sha_id>', methods=['DELETE'])
+def delete_task(sha_id):
+    sha = [sha for sha in events if sha['sha_id'] == sha_id]
+    if len(sha) == 0:
+        abort(404)
+    events.remove(sha[0])
+    return jsonify({'result': True})
 
 @app.errorhandler(404)
 def not_found(error):
