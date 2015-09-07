9ad8b4abbdc20a5d8d4ff4fe2f9083c57c321690
Jacob Begin <jbegin@gridcosystems.com>
2015-09-06 20:36:34 -0400

    Completed question 2.

--- rest_client/test_service.py
+++ rest_client/test_service.py
@@ -3,20 +3,56 @@
 import sys
 import json
 import requests
-from netaddr import IPNetwork
+from netaddr import IPNetwork, IPAddress
 from proto.ip_event_pb2 import IpEvent
 requests.packages.urllib3.disable_warnings()
 from requests.exceptions import ConnectionError
 
 def main(argv):
+
     
+    r = requests.delete('http://localhost:5000/events')
+    print 'url: ' + r.url
+    print 'status: %s' % r.status_code
+    print 'text: %s' % r.text
+
+    print '--------------------'
+
+    bad_ips = []
+    ips = [IPAddress('192.168.1.16'), IPAddress('96.1.2.3'), IPAddress('223.5.76.1'), IPAddress('2.56.8.9'), IPAddress('96.1.2.3')]
+    for ip in ips:
+        event = IpEvent()
+        event.app_sha256 = 'deadbeef'
+        event.ip = ip.value
+        bad_ips.append(event)
+
+    for event in bad_ips:
+        headers = {"Content-Type":"application/octet-stream"}    
+        r = requests.post(
+            'http://localhost:5000/events',
+            headers=headers,
+            data=event.SerializeToString())
+        print 'url: ' + r.url
+        print 'status: %s' % r.status_code
+        if r.status_code == 201 or r.status_code == 200:
+            print 'headers: %s' % r.headers
+            print 'text: %s' % r.text
+        
+    print '--------------------'
+
     good_ips = []
     # good IPs
     for ip in IPNetwork('192.168.1.0/28').iter_hosts():
         event = IpEvent()
-        event.app_sha256='deadbeef'
-        event.ip=ip.value
+        event.app_sha256 = 'deadbeef'
+        event.ip = ip.value
         good_ips.append(event)
+
+    event = IpEvent()
+    event.app_sha256 = 'deadbeef'
+    event.ip = IPAddress('192.168.1.1').value
+    good_ips.append(event)
+
 
     for event in good_ips:
         headers = {"Content-Type":"application/octet-stream"}    
@@ -29,7 +65,29 @@ def main(argv):
         if r.status_code == 201 or r.status_code == 200:
             print 'headers: %s' % r.headers
             print 'text: %s' % r.text
-
+    
+    print '--------------------'
+    
+    bad_ips = []
+    ips = [IPAddress('96.1.2.3'), IPAddress('223.5.76.1'), IPAddress('2.56.8.9'), IPAddress('96.1.2.3')]
+    for ip in ips:
+        event = IpEvent()
+        event.app_sha256 = 'deadbeef'
+        event.ip = ip.value
+        bad_ips.append(event)
+    
+    for event in bad_ips:
+        headers = {"Content-Type":"application/octet-stream"}    
+        r = requests.post(
+            'http://localhost:5000/events',
+            headers=headers,
+            data=event.SerializeToString())
+        print 'url: ' + r.url
+        print 'status: %s' % r.status_code
+        if r.status_code == 201 or r.status_code == 200:
+            print 'headers: %s' % r.headers
+            print 'text: %s' % r.text
+    
     print '--------------------'
 
     ip = IPNetwork('192.168.1.1/28')
@@ -45,87 +103,81 @@ def main(argv):
     print 'event: %s' % event2
     
     print '--------------------'
-
-    #r = requests.delete('http://localhost:5000/events')
-    #print 'url: ' + r.url
-    #print 'status: %s' % r.status_code
-    #print 'text: %s' % r.text
-    #
-    #print '--------------------'
-    #
-    #headers = {'Accept':'application/json'}
-    #r = requests.get('http://localhost:5000/events/123fff', headers=headers)
-    #print 'url: ' + r.url
-    #print 'status: %s' % r.status_code
-    #print 'headers: %s' % r.headers
-    #print 'text: %s' % r.text
-    #print 'json response: %s' % r.json()
-    #
-    #print '--------------------'
-    #
-    #headers = {"Content-Type":"application/octet-stream"}    
-    #r = requests.post(
-    #    'http://localhost:5000/events',
-    #    headers=headers,
-    #    data=event1.SerializeToString())
-    #print 'url: ' + r.url
-    #print 'status: %s' % r.status_code
-    #if r.status_code == 201 or r.status_code == 200:
-    #    print 'headers: %s' % r.headers
-    #    print 'text: %s' % r.text
-    #    #print 'json response: %s' % r.json()
-    #
-    #print '--------------------'
-    #
-    #headers = {"Content-Type":"application/octet-stream"}    
-    #r = requests.post(
-    #    'http://localhost:5000/events',
-    #    headers=headers,
-    #    data=event1.SerializeToString())
-    #print 'url: ' + r.url
-    #print 'status: %s' % r.status_code
-    #if r.status_code == 201 or r.status_code == 200:
-    #    print 'headers: %s' % r.headers
-    #    print 'text: %s' % r.text
-    #    #print 'json response: %s' % r.json()
-    #
-    #print '--------------------'
-    #
-    #headers = {"Content-Type":"application/octet-stream"}    
-    #r = requests.post(
-    #    'http://localhost:5000/events',
-    #    headers=headers,
-    #    data=event2.SerializeToString())
-    #print 'url: ' + r.url
-    #print 'status: %s' % r.status_code
-    #if r.status_code == 201 or r.status_code == 200:
-    #    print 'headers: %s' % r.headers
-    #    print 'text: %s' % r.text
-    #    #print 'json response: %s' % r.json()
-    #
-    #print '--------------------'
-    #
-    #headers = {'Accept':'application/json'}
-    #r = requests.get('http://localhost:5000/events/123fff', headers=headers)
-    #print 'url: ' + r.url
-    #print 'status: %s' % r.status_code
-    #print 'headers: %s' % r.headers
-    #print 'text: %s' % r.text
-    #print 'json response: %s' % r.json()
-    #
-    #print '--------------------'
-    #
-    #headers = {'Accept':'application/json'}
-    #r = requests.get('http://localhost:5000/events/123aaa', headers=headers)
-    #print 'url: ' + r.url
-    #print 'status: %s' % r.status_code
-    #print 'headers: %s' % r.headers
-    #print 'text: %s' % r.text
-    #print 'json response: %s' % r.json()
-    #
-    #print '--------------------'
-
-    r = requests.get('http://localhost:5000/events')
+        
+    headers = {'Accept':'application/json'}
+    r = requests.get('http://localhost:5000/events/123fff', headers=headers)
+    print 'url: ' + r.url
+    print 'status: %s' % r.status_code
+    print 'headers: %s' % r.headers
+    print 'text: %s' % r.text
+    print 'json response: %s' % r.json()
+    
+    print '--------------------'
+    
+    headers = {"Content-Type":"application/octet-stream"}    
+    r = requests.post(
+        'http://localhost:5000/events',
+        headers=headers,
+        data=event1.SerializeToString())
+    print 'url: ' + r.url
+    print 'status: %s' % r.status_code
+    if r.status_code == 201 or r.status_code == 200:
+        print 'headers: %s' % r.headers
+        print 'text: %s' % r.text
+        #print 'json response: %s' % r.json()
+    
+    print '--------------------'
+    
+    headers = {"Content-Type":"application/octet-stream"}    
+    r = requests.post(
+        'http://localhost:5000/events',
+        headers=headers,
+        data=event1.SerializeToString())
+    print 'url: ' + r.url
+    print 'status: %s' % r.status_code
+    if r.status_code == 201 or r.status_code == 200:
+        print 'headers: %s' % r.headers
+        print 'text: %s' % r.text
+        #print 'json response: %s' % r.json()
+    
+    print '--------------------'
+    
+    headers = {"Content-Type":"application/octet-stream"}    
+    r = requests.post(
+        'http://localhost:5000/events',
+        headers=headers,
+        data=event2.SerializeToString())
+    print 'url: ' + r.url
+    print 'status: %s' % r.status_code
+    if r.status_code == 201 or r.status_code == 200:
+        print 'headers: %s' % r.headers
+        print 'text: %s' % r.text
+        #print 'json response: %s' % r.json()
+    
+    print '--------------------'
+    
+    headers = {'Accept':'application/json'}
+    r = requests.get('http://localhost:5000/events/123fff', headers=headers)
+    print 'url: ' + r.url
+    print 'status: %s' % r.status_code
+    print 'headers: %s' % r.headers
+    print 'text: %s' % r.text
+    print 'json response: %s' % r.json()
+    
+    print '--------------------'
+    
+    headers = {'Accept':'application/json'}
+    r = requests.get('http://localhost:5000/events/123aaa', headers=headers)
+    print 'url: ' + r.url
+    print 'status: %s' % r.status_code
+    print 'headers: %s' % r.headers
+    print 'text: %s' % r.text
+    print 'json response: %s' % r.json()
+    
+    print '--------------------'
+    
+    headers = {'Accept':'application/json'}
+    r = requests.get('http://localhost:5000/events/deadbeef', headers=headers)
     print 'url: ' + r.url
     print 'status: %s' % r.status_code
     print 'headers: %s' % r.headers
@@ -134,15 +186,15 @@ def main(argv):
     
     print '--------------------'
 
-    #headers = {'Accept':'application/json'}
-    #r = requests.get('http://localhost:5000/events/123aaa', headers=headers)
-    #print 'url: ' + r.url
-    #print 'status: %s' % r.status_code
-    #print 'headers: %s' % r.headers
-    #print 'text: %s' % r.text
-    #print 'json response: %s' % r.json()
-    #
-    #print '--------------------'
+    headers = {'Accept':'application/json'}
+    r = requests.get('http://localhost:5000/events/123aaa', headers=headers)
+    print 'url: ' + r.url
+    print 'status: %s' % r.status_code
+    print 'headers: %s' % r.headers
+    print 'text: %s' % r.text
+    print 'json response: %s' % r.json()
+    
+    print '--------------------'
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- rest_service/rest-server.py
+++ rest_service/rest-server.py
@@ -10,10 +10,17 @@ app_shas = {}
 
 @app.route('/events', methods=['GET'])
 def get_events():
-    return jsonify(app_shas)
+    """
+    Return all events descriptions for all app_sha256 values
+    """
+    if 'application/json' in request.headers['Accept']:
+        return jsonify(app_shas)
 
 @app.route('/events/<sha_id>', methods=['GET'])
 def get_event(sha_id):
+    """
+    Return event description for specified app_sha_256
+    """
     if 'application/json' in request.headers['Accept']:
         try:
             app_shas[sha_id]
@@ -26,31 +33,55 @@ def get_event(sha_id):
 
 @app.route('/events', methods=['POST'])
 def create_event():
+    """
+    Updating event descriptions for an app_sha256.  Assuming all IPs are good
+    untl at least a range of IPs reaches 14 (/28 network block).  Once a range
+    of IPs reaches a total of 14, all other IPs are considered bad.  If
+    multiple ranges of IPs reach 14, this simple processing will not be able to
+    determine while /28 block is the valid one.
+    """
     if 'application/octet-stream' in request.headers['Content-Type']:
 
+        # Protobuf decoding
         event = IpEvent()
         event.ParseFromString(request.data)
         sha_id = str(event.app_sha256)
         ip = str(IPAddress(event.ip))
         
+        # Update event description of an existing app_sha256
         if sha_id in app_shas:
             app_shas[sha_id]['count'] = app_shas[sha_id]['count'] + 1
-            app_shas[sha_id]['good_ips'].append(ip)
+            # Only add if IP is not already in the list
+            if str(ip) not in app_shas[sha_id]['good_ips']:
+                app_shas[sha_id]['good_ips'].append(str(ip))
+        # Create an event description instance for a new app_sha256
         else:
             app_shas[sha_id]={'count':1,'good_ips':[],'bad_ips':[]}
-            app_shas[sha_id]['good_ips'].append(ip)
+            # Only add if IP is not already in the list
+            if str(ip) not in app_shas[sha_id]['good_ips']:
+                app_shas[sha_id]['good_ips'].append(str(ip))
         
+        # Combined list of all IPs good and bad
         ips = app_shas[sha_id]['good_ips'] + app_shas[sha_id]['bad_ips']
+        # Convert to a set
         ip_set = IPSet(ips)
+        # Get the minimal number of IP ranges
         for ip_range in ip_set.iter_ipranges():
-            print ip_range
-            print ip_range.size
+            # Does one of te ranges have 14 IP address in it?
             if ip_range.size == 14:
+                # Thas is now our good /28 block of IP addresses
                 good_range = ip_range
-        
-        for ip in good_range:
-            print ip, ip in ip_set
-        
+                app_shas[sha_id]['good_ips'] = []
+
+                for ip in ip_set:
+                    # Populate good_ips with the range of 14 'good' IP addresses
+                    if ip in good_range:
+                        if str(ip) not in app_shas[sha_id]['good_ips']:
+                            app_shas[sha_id]['good_ips'].append(str(ip))
+                    # Put the rest of IP addresses in bad_ips
+                    else:
+                        if str(ip) not in app_shas[sha_id]['bad_ips']:
+                            app_shas[sha_id]['bad_ips'].append(str(ip))
         
         return 'Received event...'
     else:
@@ -58,12 +89,17 @@ def create_event():
 
 @app.route('/events', methods=['DELETE'])
 def delete_events():
-
+    """
+    Delete all app_sha256 event description data
+    """
     app_shas.clear()    
     return jsonify({'result': True})
 
 @app.route('/events/<sha_id>', methods=['DELETE'])
 def delete_event(sha_id):
+    """
+    Delete specified app_sha256 event descriptions
+    """
     try:
         del app_shas[sha_id]
     except KeyError:
